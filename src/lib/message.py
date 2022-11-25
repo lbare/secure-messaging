610a2a66955d47a541d4fb8a6a66ec36ea6cac5e
Levi <47727379+lbare@users.noreply.github.com>
2022-11-25 14:03:11 -0800

    working on test

--- src/lib/message.py
+++ src/lib/message.py
@@ -3,6 +3,12 @@ import generate_hmac
 import generate_keys
 from datetime import datetime
 
+message_type = {
+    "login_request",
+    "delete_account_request",
+    "send_message"
+}
+
 
 def generate_timestamp():
     epoch_time = datetime(1970, 1, 1)
@@ -13,13 +19,15 @@ def generate_timestamp():
 
 class Message:
     def __init__(self, msg_content, hmac, shared_client_key, shared_server_key,
-                 msg_type=None, recipient_id=None):
+                 msg_type=None, sender_id=None, recipient_id=None, encrypted_payload=None):
         self.msg_content = msg_content
         self.hmac = hmac
         self.shared_client_key = shared_client_key
         self.shared_server_key = shared_server_key
         self.msg_type = msg_type
+        self.sender_id = sender_id
         self.recipient_id = recipient_id
+        self.encrypted_payload = encrypted_payload
 
     def client_to_server(self):
         client_payload = f"hmac:{self.hmac}, " \
@@ -36,7 +44,47 @@ class Message:
         return encrypted_server_payload
 
     def server_to_client(self):
-        pass
+        recipient_payload = f"sender_id:{self.sender_id}, " \
+                            f"timestamp:{generate_timestamp()}" \
+                            f"payload:{self.encrypted_payload}"
+        return recipient_payload
+
+
+if __name__ == "__main__":
+    # Alice sending to Bob
+
+    # user IDs
+    alice_id = 123
+    bob_id = 456
+
+    # generate DH
+    alice = generate_keys.generate_new_DH()
+    bob = generate_keys.generate_new_DH()
+    client = generate_keys.generate_new_DH()
+
+    # generate private keys
+    alice_private_key = alice.get_private_key()
+    bob_private_key = bob.get_private_key()
+    client_private_key = client.get_private_key()
+
+    # generate shared keys
+    alice_bob_shared_key = generate_keys.generate_shared_key(alice, bob)
+    alice_server_shared_key = generate_keys.generate_shared_key(alice, client)
+    bob_server_shared_key = generate_keys.generate_shared_key(bob, client)
+
+    # message parameters
+    msg_content_test = "hello there"
+    hmac_test = generate_hmac.generate_new_hmac(alice_bob_shared_key, msg_content_test)
+    msg_type_test = "send_message"
+    sender_id_test = alice_id
+    recipient_id_test = bob_id
+
+    # create message
+    msg_to_server = Message(msg_content_test,
+                  hmac_test, alice_bob_shared_key, alice_server_shared_key,
+                  msg_type=msg_type_test,
+                  sender_id=sender_id_test, recipient_id=recipient_id_test)
+    print(msg_to_server)
 
 
 
